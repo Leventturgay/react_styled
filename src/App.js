61c7d87b91295ce42bb17548618018dd7db21ce2
Turgay Levent <turgayleventt@gmail.com>
2022-10-26 21:48:41 +0300

    v4

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const theme = {
     body: "#fff",
     footer: "#8A1C4A",
   },
+  responsive: "768px",
 };
 const App = () => {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Button from "./styles/Button.styled";
-import StyledHeader from "./styles/Header.styled";
+import Flex from "./styles/Flex.styled";
+import StyledHeader, { Image, Logo, Nav } from "./styles/Header.styled";
 
 const Header = () => {
   return (
     <StyledHeader>
-      <Button color="#A62440"> Apply Courses </Button>
-      <Button bg="#A62440"> Talk To Adviser </Button>
+      <Nav>
+        <Logo src="./images./logo.png" />
+        <div>
+          <Button color="#A62440"> Apply Courses </Button>
+          <Button bg="#A62440"> Talk To Adviser </Button>
+        </div>
+      </Nav>
+      <Flex>
+        <div>
+          <h1> The IT Career</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+            cupiditate aliquam voluptate pariatur dicta a ipsa doloribus
+            quaerat, voluptatibus qui.
+          </p>
+          <Button bg="#A62440"> Start New Career </Button>
+        </div>
+        <Image src="./images/hero.jpg" />
+      </Flex>
     </StyledHeader>
   );
 };
